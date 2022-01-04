41a2ab898dc7c4a9a7a6f0a9780d346cf5bd21d7
Santiago_Albisser <salbisserc@eafit.edu.co>
2022-01-03 21:56:04 -0500

    Movies App Start

---
+++ MoviesApp/movies_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../screens/screens.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Movies App',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => const HomeScreen(),
+        'details': (_) => const DetailsScreen()
+      },
+      
+    );
+  }
+}
